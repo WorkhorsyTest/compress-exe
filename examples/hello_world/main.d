2911fdd2783772b73438a5fb9a3975f2d0ad37e3
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-04-22 12:28:47 -0700

    Added basic example.

---
+++ examples/hello_world/main.d
@@ -0,0 +1,14 @@
+// Copyright (c) 2017 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// Boost Software License - Version 1.0
+// Store a D program compressed inside another D program
+// https://github.com/workhorsy/compress-exe
+
+
+import std.stdio;
+
+
+int main() {
+	stdout.writefln("Da actual program and stuff ...");
+
+	return 0;
+}
--- source/main.d
+++ source/main.d
@@ -3,16 +3,22 @@
 // Store a D program compressed inside another D program
 // https://github.com/workhorsy/compress-exe
 
-
+import std.stdio;
+import std.path;
+import std.array;
 import compress_exe;
 import compress;
 
 
-int main() {
+int main(string[] args) {
 	// Get a list of all the files to store
-	string[] file_names = [
-		"README.md",
-	];
+	string[] file_names = args[1 .. $];
+
+	// Convert all the file paths to be absolute
+	for (int i=0; i<file_names.length; i++) {
+		file_names[i] = file_names[i].absolutePath().asNormalizedPath().array;
+	}
+	//stdout.writefln("!!! file_names:%s", file_names);
 
 	// Generate a D file that contains all the files as compressed blobs
 	GenerateWrapper(file_names, CompressionType.Zlib);
